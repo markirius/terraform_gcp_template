e55854c1f0659d8032e86ce78cf12b254898967a
Markirius <markbombomb@gmail.com>
2020-09-14 07:14:58 -0300

    some fix on code and added more variablesw

--- main.tf
+++ main.tf
@@ -2,25 +2,25 @@
 provider "google" {
 	credentials = file(var.credentials_file_path)
 	project = var.org_id
-	region	= var.region
+	region = var.region
 }
 
 //Creating First VPC Network
 resource "google_compute_network" "network" {
-	name										= "${var.project_name}-${var.env}-network"
-	description							= "${var.project_name} Network"
-	project									= var.org_id
-  routing_mode            = "REGIONAL"
+	name = "${var.project_name}-${var.env}-network"
+	description = "${var.project_name} Network"
+	project = var.org_id
+  routing_mode = "REGIONAL"
   auto_create_subnetworks = false
 }
 
 //Creating Subnetwork for First VPC
 resource "google_compute_subnetwork" "subnet" {
-	name          = "${var.project_name}-${var.env}-subnet"
+	name = "${var.project_name}-${var.env}-subnet"
 	ip_cidr_range = var.subnet_range
-	project       = var.org_id
-	region        = var.region
-  network       = google_compute_network.network.id
+	project = var.org_id
+	region = var.region
+  network = google_compute_network.network.id
 
   depends_on = [
     google_compute_network.network
@@ -30,14 +30,14 @@ resource "google_compute_subnetwork" "subnet" {
 
 //Creating Firewall for First VPC Network
 resource "google_compute_firewall" "network-firewall" {
-	name    = "${var.project_name}-${var.env}-network-firewall"
+	name = "${var.project_name}-${var.env}-network-firewall"
   network = google_compute_network.network.id
 
 	source_ranges = ["0.0.0.0/0"]
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443", "9000"]
+    ports = ["22", "80", "443", "9000"]
   }
 
   source_tags = ["${var.project_name}", "network"]
@@ -49,19 +49,19 @@ resource "google_compute_firewall" "network-firewall" {
 }
 
 resource "google_compute_firewall" "subnet-firewall" {
-	name    = "${var.project_name}-${var.env}-subnetwork-firewall"
+	name = "${var.project_name}-${var.env}-subnetwork-firewall"
   network = google_compute_network.network.id
 
 	source_ranges = ["${var.subnet_range}"]
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "9000", "8000", "9001", "80", "7946", "53", "2377", "2376"]
+    ports = ["22", "9000", "8000", "9001", "80", "7946", "53", "2377", "2376"]
   }
 
 	allow {
 		protocol = "udp"
-		ports		 = ["7946", "4789", "53"]
+		ports = ["7946", "4789", "53"]
 	}
 
   source_tags = ["${var.project_name}", "subnet"]
@@ -72,29 +72,29 @@ resource "google_compute_firewall" "subnet-firewall" {
 }
 
 resource "google_compute_address" "managers_addresses" {
-	count				 = var.manager_count
-  name         = "${var.project_name}-${var.env}-manager-${count.index}"
-  subnetwork   = google_compute_subnetwork.subnet.id
+	count = var.manager_count
+  name = "${var.project_name}-${var.env}-manager-${count.index}"
+  subnetwork = google_compute_subnetwork.subnet.id
   address_type = "INTERNAL"
-  address      = "10.0.4.${count.index+2}"
-  region       = "us-central1"
+  address = "10.0.4.${count.index+2}"
+  region = "us-central1"
 }
 
 resource "google_compute_address" "workers_addresses" {
-	count				 = var.worker_count
-  name         = "${var.project_name}-${var.env}-worker-${count.index}"
-  subnetwork   = google_compute_subnetwork.subnet.id
+	count = var.worker_count
+  name = "${var.project_name}-${var.env}-worker-${count.index}"
+  subnetwork = google_compute_subnetwork.subnet.id
   address_type = "INTERNAL"
-  address			 = "10.0.4.${count.index+10}"
-  region       = "us-central1"
+  address = "10.0.4.${count.index+10}"
+  region = "us-central1"
 }
 
 # Create VM instance
 resource "google_compute_instance" "manager" {
-	count				 = var.manager_count
-	name         = "${var.project_name}-${var.env}-manager-${count.index}"
+	count = var.manager_count
+	name = "${var.project_name}-${var.env}-manager-${count.index}"
   machine_type = "n1-standard-1"
-  zone         = var.region_zone
+  zone = var.region_zone
 
   tags = ["http-server", "https-server"]
 
@@ -120,26 +120,26 @@ resource "google_compute_instance" "manager" {
   }
 
   provisioner "file" {
-    source      = var.install_script_src_path
+    source = var.install_script_src_path
     destination = var.install_script_dest_path
 
     connection {
-      host        = self.network_interface.0.access_config.0.nat_ip
-      type        = "ssh"
-      user        = "root"
+      host = self.network_interface.0.access_config.0.nat_ip
+      type = "ssh"
+      user = "root"
       private_key = file(var.private_key_path)
-      agent       = false
+      agent = false
     }
 
 	}
 
   provisioner "remote-exec" {
     connection {
-      host        = self.network_interface.0.access_config.0.nat_ip
-      type        = "ssh"
-      user        = "root"
+      host = self.network_interface.0.access_config.0.nat_ip
+      type = "ssh"
+      user = "root"
       private_key = file(var.private_key_path)
-      agent       = false
+      agent = false
     }
 
     inline = [
@@ -154,18 +154,18 @@ resource "google_compute_instance" "manager" {
 }
 
 resource "google_compute_instance" "worker" {
-	count				 = var.worker_count
-	name         = "${var.project_name}-${var.env}-worker-${count.index}"
+	count = var.worker_count
+	name = "${var.project_name}-${var.env}-worker-${count.index}"
   machine_type = "n1-standard-1"
-  zone         = var.region_zone
+  zone = var.region_zone
 
   tags = ["worker", "${var.project_name}"]
 
   boot_disk {
     initialize_params {
       image = var.project_image
-			size = 40
-			type = "pd-standard" //options: pd-ssd or pd-standard
+			size = var.project_boot_disk_size
+			type = var.project_boot_disk_type
     }
   }
 
@@ -183,26 +183,26 @@ resource "google_compute_instance" "worker" {
   }
 
   provisioner "file" {
-    source      = var.install_script_src_path
+    source = var.install_script_src_path
     destination = var.install_script_dest_path
 
     connection {
-      host        = self.network_interface.0.access_config.0.nat_ip
-      type        = "ssh"
-      user        = "root"
+      host = self.network_interface.0.access_config.0.nat_ip
+      type = "ssh"
+      user = "root"
       private_key = file(var.private_key_path)
-      agent       = false
+      agent = false
     }
 
 	}
 
   provisioner "remote-exec" {
     connection {
-      host        = self.network_interface.0.access_config.0.nat_ip
-      type        = "ssh"
-      user        = "root"
+      host = self.network_interface.0.access_config.0.nat_ip
+      type = "ssh"
+      user = "root"
       private_key = file(var.private_key_path)
-      agent       = false
+      agent = false
     }
 
     inline = [
--- variables.tf
+++ variables.tf
@@ -13,52 +13,62 @@ variable "org_id" {
 
 variable "credentials_file_path" {
 	description = "Location of the credentials to use. (auth json file)"
-	default     = "auth_file.json"
+	default = "auth_file.json"
 }
 
 variable "vpc_routing_mode" {
 	description = "VPC routing mode, can be REGIONAL (default) or GLOBAL"
-	default			= "REGIONAL"
+	default	= "REGIONAL"
 }
 
 variable "env" {
 	description = "Set type of environment, like staging, production... etc"
-	default			= "development"
+	default	= "development"
 }
 
 variable "project_name" {
 	description = "Name project"
-	default			= "project"
+	default	= "project"
 }
 
 variable "subnet_range" {
 	description = "Subnetwork ip range"
-	default			= "10.0.4.0/24"
+	default	= "10.0.4.0/24"
 }
 
 variable "install_script_src_path" {
 	description = "Path to install script within this repository"
-	default     = "./scripts/docker.sh"
+	default = "./scripts/docker.sh"
 }
 
 variable "install_script_dest_path" {
   description = "Path to put the install script on each destination resource"
-  default     = "/tmp/docker.sh"
+  default = "/tmp/docker.sh"
 }
 
 variable "public_key_path" {
   description = "Path to file containing public key"
-  default     = "~/.ssh/rsa_id.pub"
+  default = "~/.ssh/rsa_id.pub"
 }
 
 variable "private_key_path" {
   description = "Path to file containing private key"
-  default     = "~/.ssh/rsa_id"
+  default = "~/.ssh/rsa_id"
 }
 
 variable "project_image" {
 	description = "Image used by whole project"
-	default			= "debian-cloud/debian-10"
+	default	= "debian-cloud/debian-10"
+}
+
+variable "project_boot_disk_size" {
+	description = "Boot disk size"
+	default = 10 //GiB
+}
+
+variable "project_boot_disk_type" {
+	description = "Boot disk type"
+	default = "pd-standard" //options: pd-ssd or pd-standard
 }
 
 variable "manager_count" {
